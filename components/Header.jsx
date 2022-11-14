e5adb2355e3bd647ddc7a6f2a3c305d499e84aed
camilo micheletto <camilomichelettors@gmail.com>
2022-11-14 02:46:26 -0300

    feat: add click and autoplay to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,20 @@
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export const Header = ({ videos }) => {
+  const router = useRouter();
   return (
     <header className="header">
       <Splide
         aria-label="new filmes presentation"
         className="header__slider"
         options={{
+          autoplay: true,
+          interval: 5000,
           classes: {
-            pagination: 'splide__pagination header__pagination',
+            pagination: "splide__pagination header__pagination",
           },
           gap: "2ch",
           padding: "5rem",
@@ -17,14 +22,21 @@ export const Header = ({ videos }) => {
         }}
       >
         {videos?.map((video, index) => {
+          const href = `/video/${video.slug}`;
           return (
-            <SplideSlide className="header__slide" key={index}>
-              <Image
-                alt={video.title}
-                className="header__image"
-                src={video.thumbnail?.url}
-                fill
-              />
+            <SplideSlide
+              className="header__slide"
+              key={index}
+              onClick={() => router.push(href)}
+            >
+              <Link href={href}>
+                <Image
+                  alt={video.title}
+                  className="header__image"
+                  src={video.thumbnail?.url}
+                  fill
+                />
+              </Link>
             </SplideSlide>
           );
         })}
